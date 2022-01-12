743c15f3fef5db5ab6880eabb046171d0c20f0d9
TotalZero <radek.solekr@icloud.com>
2022-01-12 15:42:04 +0100

    dodanie konfiguracji projektu speechkeys

---
+++ SpeechKeys/main.js
@@ -0,0 +1,13 @@
+const {app, BrowserWindow} = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+ width: 800,
+ height: 600
+    })
+    win.loadFile('index.htm')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
